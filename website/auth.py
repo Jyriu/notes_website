a10c7907ee88688a6af229e7dd288c5f41f8c85f
Jyriu <sami.yezza@gmail.com>
2024-03-17 02:11:03 +0100

    commit inscription à fix

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('views', __name__)
 
@@ -16,19 +19,23 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstname = request.form.get('firstname')
+        first_name = request.form.get('first_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        if email is None or len(email) < 4:
             flash("L'adresse email doit contenir au moins 4 caractères.", category='error')
-        elif len(firstname) < 2:
+        elif first_name is None or len(first_name) < 2:
             flash("Le prénom doit contenir au moins 2 caractères.", category='error')
-        elif password1 != password2:
+        elif password1  is None or password2 is None or password1 != password2:
             flash("Les mots de passe ne correspondent pas.", category='error')
-        elif len(password1) < 7:
+        elif password1 is None or len(password1) < 7:
             flash("Le mot de passe doit contenir au moins 7 caractères", category='error')
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Inscription réussie!", category='success')
-            # ajouter l'utilisateur à la base de données
+            return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
